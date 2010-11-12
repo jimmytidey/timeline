6a576ba56fab4d44e99c3b5123add4d80fbd64a0
Andrew Nagi <andrew.nagi@gmail.com>
2010-11-12 10:58:49 +0000

    Fix for problem with PostgreSQL on heroku

--- db/migrate/20101109171418_change_timeline_charts_granularity_from_string_to_integer.rb
+++ db/migrate/20101109171418_change_timeline_charts_granularity_from_string_to_integer.rb
@@ -1,9 +1,11 @@
 class ChangeTimelineChartsGranularityFromStringToInteger < ActiveRecord::Migration
   def self.up
-    change_column :timeline_charts, :granularity, :integer
+    remove_column :timeline_charts, :granularity, :string
+    add_column :timeline_charts, :granularity, :integer
   end
 
   def self.down
-    change_column :timeline_charts, :granularity, :string
+    remove_column :timeline_charts, :granularity, :integer
+    add_column :timeline_charts, :granularity, :string
   end
 end
