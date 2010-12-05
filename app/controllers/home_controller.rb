7d5126883ac2cbdd3ae8050e93ce36272bf5d88c
Andrew Nagi <andrew.nagi@gmail.com>
2010-12-05 20:21:41 +0000

    Remove logging. Add env var for janrain application name.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,6 @@ class HomeController < ApplicationController
   protect_from_forgery :only => [:create, :update, :destroy] 
 
   def index 
-    logger.fatal( "  **********************  " )
-    logger.fatal( url_for(:controller => :users, :action => :create, :only_path => false, :forward => 'new_timeline') )
-    logger.fatal( "  **********************  " )
     @top_charts = TimelineChart.top_charts(20)
     if current_user
       @user_charts  = current_user.timeline_charts
