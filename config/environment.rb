e4494f8b4a6923e6b25b83ed88003e9e53ea73f0
jimmytidey <jimmytidey@JImmy-Tideys-MacBook-Pro.local>
2011-01-05 23:12:49 +0000

    more debug

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Timeline::Application.initialize!
 
-ActiveSupport::CoreExtensions::Time::Conversions::DATE_  
-  FORMATS.update(:default => '%Y-%m-%d %H:%M:%S')  
-ActiveSupport::CoreExtensions::Date::Conversions::DATE_    
-  FORMATS.update(:default => '%Y-%m-%d')  
