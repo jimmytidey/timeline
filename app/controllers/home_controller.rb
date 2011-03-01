9afd488f418478542904fe81364c117126f77360
jimmytidey <jimmytidey@JImmy-Tideys-MacBook-Pro-2.local>
2011-03-01 19:19:15 +0000

    modifying top timelines so they must have atleast two events

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   protect_from_forgery :only => [:create, :update, :destroy] 
 
   def index 
-    @top_charts = TimelineChart.top_charts(20)
+    @top_charts = TimelineChart.top_charts(200)
     if current_user
       @user_charts  = current_user.timeline_charts
     end
